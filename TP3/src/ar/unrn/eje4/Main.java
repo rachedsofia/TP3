e15075b729cb9af90149cb19e6733a39c96980ae
msofi <msofiarached@gmail.com>
2023-04-12 19:59:41 -0300

    Actualizado eje4

--- TP3/src/ar/unrn/eje4/Main.java
+++ TP3/src/ar/unrn/eje4/Main.java
@@ -4,24 +4,22 @@ import org.jdbi.v3.core.Jdbi;
 
 public class Main {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    Jdbi jdbi = Jdbi.create("jdbc:hsqldb:mem;create=true");
+		Jdbi jdbi = Jdbi.create("jdbc:hsqldb:mem;create=true");
 
-    new SetUpDatabase(jdbi).setUp();
+		new SetUpDatabase(jdbi).setUp();
 
-    var repo = new PersonaRepository(jdbi);
-    var personas = repo.buscarPorNombre("Vla");
+		var repo = new PersonaRepository(jdbi);
+		var personas = repo.buscarPorNombre("Vla");
 
-    if (personas != null) {
-      for (Persona persona : personas) {
-        System.out.println(persona.nombre() + " " + persona.apellido());
-      }
-    }
+		for (Persona persona : personas) {
+			System.out.println(persona.nombre() + " " + persona.apellido());
 
-    var persona = repo.buscarId(1L);
-    if (persona != null) {
-      System.out.println(persona.nombre() + " " + persona.apellido());
-    }
-  }
+		}
+
+		var persona = repo.buscarId(1L);
+
+		persona.ifPresent(p -> System.out.printf(p.nombre(), p.apellido()));
+	}
 }
